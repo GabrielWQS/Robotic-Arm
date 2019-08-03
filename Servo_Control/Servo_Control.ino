19347e67b0e99fcbd938cb5b413f2089d075f338
Gabriel Wallace <gabriel.wallace.qs@gmail.com>
2019-08-02 22:06:11 -0300

    Modificações para controle de 7 servos através de envio pela serial

--- Servo_Control/Servo_Control.ino
+++ Servo_Control/Servo_Control.ino
@@ -15,12 +15,16 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
 #define SERVO_3_MAX  535
 #define SERVO_4_MIN  110 
 #define SERVO_4_MAX  535
+#define SERVO_5_MIN  110 
+#define SERVO_5_MAX  535
+#define SERVO_6_MIN  110 
+#define SERVO_6_MAX  535
 
 
 // our servo # counter
 
-int position_0 = 0, position_1 = 0, position_2 = 0, position_3 = 0;
-double pulselength_0 = 0, pulselength_1 = 0, pulselength_2 = 0, pulselength_3 = 0; 
+int position_0 = 0, position_1 = 0, position_2 = 0, position_3 = 0, position_4 = 0, position_5 = 0, position_6 = 0;
+double pulselength_0 = 0, pulselength_1 = 0, pulselength_2 = 0, pulselength_3 = 0, pulselength_4 = 0, pulselength_5 = 0, pulselength_6 = 0; 
 
 int cont = 0;
 bool enable = false, servoStart = false, servoEnd = false;
@@ -77,6 +81,9 @@ void setNextPosition(String myServoString)
   position_1 = getPosition(cont, myServoString); 
   position_2 = getPosition(cont, myServoString);
   position_3 = getPosition(cont, myServoString);  
+  position_4 = getPosition(cont, myServoString);  
+  position_5 = getPosition(cont, myServoString);  
+  position_6 = getPosition(cont, myServoString);  
 }
 
 int getPosition(int i, String v)
@@ -115,21 +122,33 @@ void loop()
         pulselength_1 = map(position_1, 0, 180, SERVO_1_MIN, SERVO_1_MAX);
         pulselength_2 = map(position_2, 0, 180, SERVO_2_MIN, SERVO_2_MAX);
         pulselength_3 = map(position_3, 0, 180, SERVO_3_MIN, SERVO_3_MAX);
+        pulselength_4 = map(position_4, 0, 180, SERVO_4_MIN, SERVO_4_MAX);
+        pulselength_5 = map(position_5, 0, 180, SERVO_5_MIN, SERVO_5_MAX);
+        pulselength_6 = map(position_6, 0, 180, SERVO_6_MIN, SERVO_6_MAX);
         
         //pwm.setPWM(0, 0, pulselength_0);
         //pwm.setPWM(1, 0, pulselength_1);
         //pwm.setPWM(2, 0, pulselength_2);
         //pwm.setPWM(3, 0, pulselength_3);
+        //pwm.setPWM(4, 0, pulselength_4);
+        //pwm.setPWM(5, 0, pulselength_5);
+        //pwm.setPWM(6, 0, pulselength_6);
 
-        Serial.println(position_0);
-        Serial.println(position_1);
-        Serial.println(position_2);
-        Serial.println(position_3);
+        Serial.print("Servo 0: "); Serial.print(position_0); Serial.print(" PL: "); Serial.println(pulselength_0);
+        Serial.print("Servo 1: "); Serial.print(position_1); Serial.print(" PL: "); Serial.println(pulselength_1);
+        Serial.print("Servo 2: "); Serial.print(position_2); Serial.print(" PL: "); Serial.println(pulselength_2);
+        Serial.print("Servo 3: "); Serial.print(position_3); Serial.print(" PL: "); Serial.println(pulselength_3);
+        Serial.print("Servo 4: "); Serial.print(position_4); Serial.print(" PL: "); Serial.println(pulselength_4);
+        Serial.print("Servo 5: "); Serial.print(position_5); Serial.print(" PL: "); Serial.println(pulselength_5);
+        Serial.print("Servo 6: "); Serial.print(position_6); Serial.print(" PL: "); Serial.println(pulselength_6);
 
-        Serial.println(pulselength_0);
-        Serial.println(pulselength_1);
-        Serial.println(pulselength_2);
-        Serial.println(pulselength_3);
+        
+        
+        
+        
+        
+        
+        
 
       }
   }
